b1a8fa9fb0f55be1e919c2f00525220ad6eb5e11
lbilde <ljb@easv.dk>
2019-01-24 10:58:56 +0100

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { environment } from '../environments/environment';
+import { FormsModule } from '@angular/forms';
+import { MomentModule } from 'angular2-moment';
+import {MessageService} from './message/shared/message.service';
+import {AngularFireModule} from '@angular/fire';
+import {AngularFirestoreModule} from '@angular/fire/firestore';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule, // imports firebase/firestore, only needed for database features
+    MomentModule
+  ],
+  providers: [
+    MessageService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyAjihC5R2yzsvHosA3nXx7brEdakdTUzXY',
+    authDomain: 'easv-chat-app-2019.firebaseapp.com',
+    databaseURL: 'https://easv-chat-app-2019.firebaseio.com',
+    projectId: 'easv-chat-app-2019',
+    storageBucket: 'easv-chat-app-2019.appspot.com',
+    messagingSenderId: '630753631973'
+  }
+};
